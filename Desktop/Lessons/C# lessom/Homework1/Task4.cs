c7040eace72141ae5a2d7fcbb0068e49fdefe18d
Nikolai <sealmove95@yandex.ru>
2023-01-12 00:30:03 +0300

    Task 4

---
+++ Desktop/Lessons/C# lessom/Homework1/Task4.cs
@@ -0,0 +1,15 @@
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+int num3 = Convert.ToInt32(Console.ReadLine());
+if (num1 > num2 && num1 > num3)
+  {
+    Console.WriteLine(num1);
+  }
+if (num2 > num1 && num2 > num3)
+  {
+    Console.WriteLine(num2);
+  }
+if (num3 > 1 && num3 > num2) 
+  {
+Console.WriteLine(num3);
+  }
